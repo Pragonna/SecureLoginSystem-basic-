719bbd6981da6e103d750bd14900bbb679cd8719
Pragonna <alizadeemil5@gmail.com>
2025-04-14 17:21:51 +0400

    changed

--- src/Core.Application/Abstractions/IQuery.cs
+++ src/Core.Application/Abstractions/IQuery.cs
@@ -1,13 +1,16 @@
-﻿using MediatR;
+﻿using Core.Application.Common;
+using MediatR;
+using Sprache;
 
 namespace Core.Application.Abstractions
 {
-    public interface IQuery<TResponse>:IRequest<TResponse>
+    public interface IQuery<TResponse> : IRequest<Result<TResponse, Error>> where TResponse : class, new()
     {
     }
-    
-    public interface IQueryHandler<TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+
+    public interface IQueryHandler<TRequest, TResponse> : IRequestHandler<TRequest, Result<TResponse, Error>>
         where TRequest : IQuery<TResponse>
+        where TResponse : class, new()
     {
     }
 }
